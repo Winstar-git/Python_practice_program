975d510ebfe122b390158128291c0d89407e0755
Winstar <james.canoy87@gmail.com>
2025-03-08 22:42:03 +0800

    selection limit

--- program_selection_batch1.py
+++ program_selection_batch1.py
@@ -1,3 +1,13 @@
 # Create a funtion menu for program selection
-# Create a funtion that limit the selection
+def menu():
+    def selection(prompt): # Create a funtion that limit the selection 
+        while True:
+            try:
+                value = int(input(prompt))
+                if 0 <= value <= 10:
+                    return value
+                else:
+                    print('Invalid input. Please select a number in the choices.')
+            except ValueError:
+                print("Invalid input. Please enter valid number.")
 # Create a the selection menu
