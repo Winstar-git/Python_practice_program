6d63b66958d191500eb8cde8035a149b4b75a0b2
Winstar <james.canoy87@gmail.com>
2025-03-08 23:57:21 +0800

    Inserting all practice program of batch 2

--- program_selection_batch2.py
+++ program_selection_batch2.py
@@ -1,3 +1,136 @@
+#Insert all the programs in each fuction
+
+# Prog01: Create a program that ask user to input 2 numbers.
+# Print the smaller number.
+
+def Prog01():
+    print("Prog01: Create a program that ask user to input 2 numbers.")
+    print("        Print the smaller number.")
+    print()
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    if num1 < num2:
+        print(f"{num1} is smaller number.")
+    else:
+        print(f"{num2} is smaller number.")
+
+# Prog02: Create a program that ask user to input 2 numbers.
+# Print "Not Equal" when the numbers are not the same.
+
+def Prog02(): 
+    print("Prog02: Create a program that ask user to input 2 numbers.")
+    print("        Print 'Not Equal' when the numbers are not the same.")
+    print()
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if num1 != num2:
+        print(f"{num1} and {num2} are NOt Equal")
+    else:
+        print(f"{num1} and {num2} are Equal")
+
+# Prog03: Create a program that ask user to input 2 numbers.
+# Print the difference of the two numbers.
+
+def Prog03():
+    print("Prog03: Create a program that ask user to input 2 numbers.")
+    print("        Print the difference of the two numbers.")
+    print()
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print(f"The difference of {num1} and {num2} is {num1 - num2}")
+
+# Prog04: Create a program that ask user to input 2 numbers. 
+# Print the quotient of the two numbers without decimal point.
+
+def Prog04():
+    print("Prog04: Create a program that ask user to input 2 numbers.")
+    print("        Print the quotient of the two numbers without decimal point.")
+    print()
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(f"The quotient of the two numbers is: {num1/num2:.0f}" )
+
+# Prog05: Create a program that ask user to input 2 numbers. 
+# Print the remainder of the two first number is divided by second number
+
+def Prog05():
+    print("Prog05: Create a program that ask user to input 2 numbers.")
+    print("        Print the remainder of the two first number is divided by second number")
+    print()
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print(f"The remainder of the two numbers is {num1%num2}")   
+
+# Prog06: Create a program that ask user to input 10 numbers. 
+# Printthe result of the first number minus all of the remaining numbers.
+
+def Prog06():
+    print("Prog06: Create a program that ask user to input 10 numbers.")
+    print("        Print the result of the first number minus all of the remaining numbers.")
+    print()
+    sum = 0
+    first = int(input("1. Enter a number: "))
+    for i in range(2,11):
+        num = int(input(f"{i}. Enter a number: "))
+        sum += num
+    result = first - sum
+    print(f"The result of the first number minus all of the remaining numbers: {result}")
+
+# Prog07: Create a program that ask user to input 10 numbers. 
+# Print how many are even numbers.
+
+def Prog07():
+    print("Prog07: Create a program that ask user to input 10 numbers.")
+    print("        Print how many are even numbers.")
+    print()
+    count = 0
+    for i in range(1,11):
+        num = int(input(f"{i}. Enter a number: "))
+        if num % 2 == 0:
+            count += 1
+    print(f"There are {count} even numbers")
+
+#Prog08: Create a program that print all the 
+# odd numbers starting from 0 to 100. (Use while loop)
+
+def Prog08():
+    print("Prog08: Create a program that print all the ")
+    print("        odd numbers starting from 0 to 100. (Use while loop)")
+    print()
+    num = 1
+    while num <= 100:
+        print(num, end=" ")
+        num += 2
+
+#Prog09: Create a program that print all the numbers starting 
+# from 0 to 100 except numbers ending in zero or ending five.
+
+def Prog09():
+    print("Prog09: Create a program that print all the numbers starting from") 
+    print("        0 to 100 except numbers ending in zero or ending five.")
+    print()
+    for i in range(0,101):
+        if i % 5 != 0:
+            print(i, end=" ")
+    print()
+
+# Prog10: Create a program that ask user to input 2 numbers. 
+# Print all the numbers between the two numbers.
+
+def Prog10():
+    print("Prog10: Create a program that ask user to input 2 numbers.")
+    print("        Print all the numbers between the two numbers.")
+    print()
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    if num1 < num2:
+        for i in range(num1 + 1, num2):
+            print(i, end=" ")
+    else:        
+        for i in range(num2 + 1, num1):
+            print(i, end=" ")
+
 # Create a funtion menu for program selection
 def menu():
     def selection(prompt): # Create a funtion that limit the selection 
